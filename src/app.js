8ed63d41f4efa820c4a3f7296c51a3da065e7932
Huan <huan.luchetti@gmail.com>
2023-11-08 22:10:11 -0300

    feat: open ai api stay error

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
-import express from "express";
-import {} from "dotenv/config";
+import express, { application } from "express";
 import { PrismaClient } from "@prisma/client";
+
 import validateEnv from "./utils/validateEnv.js";
 import redisClient from "./utils/connectRedis.js";
+
 import { userRouter } from "./routers/userRouter.js";
 import { interRouter } from "./routers/interactionsRouter.js";
+
+import {} from "dotenv/config";
 
 validateEnv();
 
 const prisma = new PrismaClient();
 const app = express();
+
+app.use(express.json());
+
+import { OpenAI } from "openai";
+
+const configuration = {
+  apiKey: process.env.OPENAI_API_KEY,
+};
+const openai = new OpenAI(configuration);
+const response = await openai.listEngines();
 
 async function bootstrap() {
   // Testing
